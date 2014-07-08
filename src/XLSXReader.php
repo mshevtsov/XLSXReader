293de93c74a16ba6b4775c6386d7e5666e7aab43
Mikhail Shevtsov <mikhail.shevtsov@gmail.com>
2014-07-08 19:30:54 +0400

    Update XLSXReader.php

--- src/XLSXReader.php
+++ src/XLSXReader.php
@@ -42,7 +42,7 @@ class XLSXReader {
 
 	public function __construct($filePath, $config = array()) {
 		$this->config = array_merge($this->config, $config);
-		$this->zip = new ZipArchive();
+		$this->zip = new \ZipArchive();
 		$status = $this->zip->open($filePath);
 		if($status === true) {
 			$this->parse();
